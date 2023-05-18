1a96f820ea59f9d1f8702e68701168b44ea654ef
yvromero <yvonne.romero@uds.edu.py>
2023-05-17 23:57:07 -0400

    Add la app con principios de Clean Arquitecture

---
+++ README.md
@@ -0,0 +1,12 @@
+Pasos a seguir - Preparación 
+-Crear app con vite y vanilla
+-Instalar/reconstruir los modulos de node
+`npm i`
+-Instalar el json-server como devDependencies
+`npm i json-server --save-dev`
+-Crear el back-end para un prototipo con datos mock json
+-Crear un script para el server y habilitar el path y puerto 3001
+-Levantar el server
+`npm run server`
+-Correr la app
+`npm run dev`
---
+++ main.js
@@ -0,0 +1,26 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { UsersApp } from './src/users/users-app';
+
+document.querySelector('#app').innerHTML = `
+
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Balance de usuarios</h1>
+    <div class="card">
+
+    </div>
+  
+  </div>
+`;
+
+const element = document.querySelector('.card');
+
+//BreakingbadApp( element );
+UsersApp( element );
